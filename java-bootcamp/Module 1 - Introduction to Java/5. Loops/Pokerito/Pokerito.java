9805cdf1fd1d5a5867b749aefc2b95dcf013c6b5
Gustavo Henrique Bernardo <gustavobernardo094@gmail.com>
2022-07-26 09:33:45 -0300

    Workbook exercises

--- java-bootcamp/Module 1 - Introduction to Java/5. Loops/Pokerito/Pokerito.java
+++ java-bootcamp/Module 1 - Introduction to Java/5. Loops/Pokerito/Pokerito.java
@@ -4,71 +4,156 @@ public class Pokerito {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        /*Task 2: Explain the rules
+        System.out.println(">>Let's play Pokerito. Type anything when you're ready.");
+        scan.nextLine();
 
-        >>Let's play Pokerito. Type anything when you're ready.
-        |
-        >>It's like Poker, but a lot simpler.
-        >> (new line)
-        >> • There are two players, you and the computer.
-        >> • The dealer will give each player one card.
-        >> • Then, the dealer will draw five cards (the river)
-        >> • The player with the most river matches wins!
-        >> • If the matches are equal, everyone's a winner!
-        >> (new line)
-        >> • Ready? Type anything if you are.
-        |
-        */
-        
+        System.out.println(">>It's like Poker, but a lot simpler.\n ");
+        System.out.println("\t• There are two players, you and the computer.");
+        System.out.println("\t• The dealer will give each player one card.");
+        System.out.println("\t• Then, the dealer will draw five cards (the river)");
+        System.out.println("\t• The player with the most river matches wins!");
+        System.out.println("\t• If the matches are equal, everyone's a winner!\n");
+        System.out.println("\t• Ready? Type anything if you are.");
 
-        /*Task 3: Present the user with a card
-         println 'Here's your card:'
-         <show card>
-         <new line>
-         println 'Here's the computer's card:'
-         <show computer's card>
-        */
+        String yourCard = randomCard();
+        String computerCard = randomCard();
+
+        System.out.println(
+                "Here's your card: \n" + yourCard + "\nHere's the computer's card: \n" + computerCard + "");
 
         int yourMatches = 0;
-        int computerMatches =0;
+        int computerMatches = 0;
 
-        /** Task 4 - Draw five cards
-         * 
-         * • print:  Now, the dealer will draw five cards. Press enter to continue.
-         * • The dealer will draw a card every time the user presses enter.
-         * • Before you draw a card, print the order it was drawn in:
-         *      Card 1 
-         *      <2 new lines>
-         *      <print card>
-         *      Card 2
-         *      <2 new lines>
-         *      <print card>
-         *      ...
-         */
+        System.out.println("Now, the dealer will draw five cards. Press enter to continue.");
+        scan.nextLine();
 
-        /** Task 5 - Get the winner
-         * 
-         * • Count your number of matches.
-         * • Count the computer's number of matches.
-         * • print: Your number of matches: <yourMatches>
-         * • print: Computer number of matches:  <computerMatches>
-         * 
-         * • If you have more matches, print: You win!. 
-         * • If the computer has more matches, print: The computer wins! 
-         * • If the matches are equal, print: everyone wins!.
-         */
+        for (int i = 1; i <= 5; i++) {
+            scan.nextLine();
+            String draw = randomCard();
 
-         scan.close();
+            if (draw.equals(yourCard)) {
+                yourMatches++;
+            }
+            if (draw.equals(computerCard)) {
+                computerMatches++;
+            }
+
+            System.out.println("card " + i);
+            System.out.println(draw);
+        }
+
+        System.out.println("Your number of matches: " + yourMatches + "");
+        System.out.println("Computer number of matches: " + computerMatches + "");
+
+        if (yourMatches == computerMatches) {
+            System.out.println("everyone wins!");
+        } else if (yourMatches > computerMatches) {
+            System.out.println("You win!");
+        } else {
+            System.out.println("The computer wins!.");
+        }
+
+        scan.close();
     }
 
-    /** Task 1
-     * 
-     * Function name – randomCard
-     * @return (String)
-     * 
-     * Inside the function:
-     *   1. Gets a random number between 1 and 13.
-     *   2. Returns a card that matches the random number (get the String values from cards.text).   
-     */
+    public static String randomCard() {
+        int randomNum = (int) (Math.random() * 13) + 1;
+        switch (randomNum) {
+            case 1:
+                return "   _____\n" +
+                        "  |A _  |\n" +
+                        "  | ( ) |\n" +
+                        "  |(_'_)|\n" +
+                        "  |  |  |\n" +
+                        "  |____V|\n";
+            case 2:
+                return "   _____\n" +
+                        "  |2    |\n" +
+                        "  |  o  |\n" +
+                        "  |     |\n" +
+                        "  |  o  |\n" +
+                        "  |____Z|\n";
+            case 3:
+                return "   _____\n" +
+                        "  |3    |\n" +
+                        "  | o o |\n" +
+                        "  |     |\n" +
+                        "  |  o  |\n" +
+                        "  |____E|\n";
+            case 4:
+                return "   _____\n" +
+                        "  |4    |\n" +
+                        "  | o o |\n" +
+                        "  |     |\n" +
+                        "  | o o |\n" +
+                        "  |____h|\n";
+            case 5:
+                return "   _____ \n" +
+                        "  |5    |\n" +
+                        "  | o o |\n" +
+                        "  |  o  |\n" +
+                        "  | o o |\n" +
+                        "  |____S|\n";
+            case 6:
+                return "   _____ \n" +
+                        "  |6    |\n" +
+                        "  | o o |\n" +
+                        "  | o o |\n" +
+                        "  | o o |\n" +
+                        "  |____6|\n";
+            case 7:
+                return "   _____ \n" +
+                        "  |7    |\n" +
+                        "  | o o |\n" +
+                        "  |o o o|\n" +
+                        "  | o o |\n" +
+                        "  |____7|\n";
+            case 8:
+                return "   _____ \n" +
+                        "  |8    |\n" +
+                        "  |o o o|\n" +
+                        "  | o o |\n" +
+                        "  |o o o|\n" +
+                        "  |____8|\n";
+            case 9:
+                return "   _____ \n" +
+                        "  |9    |\n" +
+                        "  |o o o|\n" +
+                        "  |o o o|\n" +
+                        "  |o o o|\n" +
+                        "  |____9|\n";
+            case 10:
+                return "   _____ \n" +
+                        "  |10  o|\n" +
+                        "  |o o o|\n" +
+                        "  |o o o|\n" +
+                        "  |o o o|\n" +
+                        "  |___10|\n";
+            case 11:
+                return "   _____\n" +
+                        "  |J  ww|\n" +
+                        "  | o {)|\n" +
+                        "  |o o% |\n" +
+                        "  | | % |\n" +
+                        "  |__%%[|\n";
+            case 12:
+                return "   _____\n" +
+                        "  |Q  ww|\n" +
+                        "  | o {(|\n" +
+                        "  |o o%%|\n" +
+                        "  | |%%%|\n" +
+                        "  |_%%%O|\n";
+
+            case 13:
+                return "   _____\n" +
+                        "  |K  WW|\n" +
+                        "  | o {)|\n" +
+                        "  |o o%%|\n" +
+                        "  | |%%%|\n" +
+                        "  |_%%%>|\n";
+            default:
+                return "";
+        }
+    }
 
 }
--- java-bootcamp/Module 1 - Introduction to Java/5. Loops/Workbook 5.11/JavaRolls.java
+++ java-bootcamp/Module 1 - Introduction to Java/5. Loops/Workbook 5.11/JavaRolls.java
@@ -1,72 +1,51 @@
 import java.util.Scanner;
 
 public class JavaRolls {
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-      
-        /* Task 1: 
-              1. print: Let's play Rolling Java. Type anything to start.
-              2. Get the user to enter any value
-        */
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
 
-        /* Task 2: 
-              1. Once the user is ready, print: 
-                    Great, here are the rules:\n
-                     - If you roll a 6 the game stops.
-                     - If you roll a 4 nothing happens.
-                     - Otherwise, you get 1 point.\n
-                    You must collect at least 3 points to win. Enter anything to roll:
-              2. Get the user to enter any value
-        */
+    System.out.println("Let's play Rolling Java. Type anything to start.");
 
-       int score = 0;
-      
-       /* 
-         Task 3
-          1. Make a while loop that runs forever
-          2. During each run, get them to enter a random value
-          
-          
-          Task 5
-           1. During each run, call the rollDice() function.
-           2. Print each dice roll like so: You rolled a <diceRoll>.           
-           
-          
-          Task 6
-            If the user rolls a 6:
-              1. print: End of game.
-              2. stop the game. 
+    scan.nextLine();
 
-            If the user rolls a 4:
-              1. print: Zero points. Keep rolling. 
+    System.out.println("Great, here are the rules:\n");
+    System.out.println("- If you roll a 6 the game stops.");
+    System.out.println("- If you roll a 4 nothing happens.");
+    System.out.println("- Otherwise, you get 1 point.\n");
+    System.out.println("You must collect at least 3 points to win. Enter anything to roll:");
+    int score = 0;
 
-            If the user rolls anything else:
-              1. update the points variable by 1
-              2. print: One point. Keep rolling. 
+    while (true) {
+      System.out.print("Enter a random number: ");
+      scan.nextLine();
 
-       */
+      int diceRoll = rollDice();
+      System.out.println("You rolled a " + diceRoll + "");
 
-      
-      /* Task 7
-      
-           1.  After the game ends, check the user's points.
-       
-                 System.out.println("\nYour score is: " + score);
-
-           2. If the score is greater than or equal to 3, print:
-                  Wow, that's lucky. You win!
-              Otherwise, print:
-                  Tough luck, you lose :( 
-      */
+      if (diceRoll == 6) {
+        System.out.println("End of game");
+        break;
+      } else if (diceRoll == 4) {
+        System.out.print("Zero points. Keep rolling.");
+      } else {
+        score++;
+        System.out.print("One point. Keep rolling.");
+      }
 
     }
-  
-  
-    /** Task 4
-     * Function name: rollDice – rolls a dice between 1 and 6
-     * @return randomNumber (int)
-     * 
-     */ 
-  
-  
+
+    System.out.println("\nYour score is: " + score);
+
+    if (score >= 3) {
+      System.out.println("Wow, that's lucky. You win!");
+    } else {
+      System.out.println("Tough luck, you lose :(");
+    }
+
+  }
+
+  public static int rollDice() {
+    return (int) (Math.random() * 6) + 1;
+  }
+
 }
