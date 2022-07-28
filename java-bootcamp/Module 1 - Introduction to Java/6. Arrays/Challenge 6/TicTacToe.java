4bba1e556bb83345ebd4ca81c710c3bb49f87aa9
Gustavo Henrique Bernardo <gustavobernardo094@gmail.com>
2022-07-28 09:03:40 -0300

    finish array challenge: build a TicTacToe game

--- java-bootcamp/Module 1 - Introduction to Java/6. Arrays/Challenge 6/TicTacToe.java
+++ java-bootcamp/Module 1 - Introduction to Java/6. Arrays/Challenge 6/TicTacToe.java
@@ -1,79 +1,202 @@
+import java.util.Arrays;
 import java.util.Scanner;
+
+import javax.swing.border.Border;
 
 public class TicTacToe {
 
-    static Scanner scan = new Scanner(System.in);
-    public static void main(String[] args) {
-        
-            System.out.println("\nLet's play tic tac toe");
+  static Scanner scan = new Scanner(System.in);
 
-            //Task 1: Create an array with three rows of '_' characters.
+  public static void main(String[] args) {
 
-            //Task 2: Call the function printBoard();
+    System.out.println("\nLet's play tic tac toe");
 
-             
-              /*
-              {  Task 3: Loop through turns.
+    // Task 1: Create an array with three rows of '_' characters.
+    char[][] board = {
+        { '_', '_', '_' },
+        { '_', '_', '_' },
+        { '_', '_', '_' },
+    };
 
-                  if (X) turn {
-                     Task 4: call askUser(). 
-                     Task 5: populate the board using askUser's return value.
-                  } else {
-                      Task 4: call askUser(). 
-                      Task 5: populate the board using askUser's return value. Then, print it.
+    // Task 2: Call the function printBoard();
+    printBoard(board);
 
-                  }
+    System.out.print("\n");
+    for (int i = 0; i < 9; i++) {
+      if (i % 2 == 0) {
+        System.out.println("Turn: X");
+        int[] arrX = askUser(board);
 
-                Task 6 - Call the function.
-                   if return value == 3 {
-                     print: X wins and break the loop
-                  } else if return value == -3 {
-                     print: O wins and break the loop
-                  }
+        board[arrX[0]][arrX[1]] = 'X';
+        printBoard(board);
 
-              } 
-              */
+      } else {
+        System.out.println("Turn: O");
+        int[] arrO = askUser(board);
 
-            scan.close();
-        }
+        board[arrO[0]][arrO[1]] = 'O';
+        printBoard(board);
+      }
+      if (checkWin(board) == 3) {
+        System.out.println("X Wins");
+        break;
+      } else if (checkWin(board) == -3) {
+        System.out.println("O Wins");
+        break;
+      } else if (i == 8) {
+        System.out.println("It's a tie!");
+        break;
+      }
 
+    }
 
-    /** Task 2 - Write a function that prints the board.
-     * Function name - printBoard()
-     * @param board (char[][])
+    /*
+     * { Task 3: Loop through turns.
      * 
-     * Inside the function:
-     *   1. print a new line.
-     *   2. print the board.
-     *      • separate each row by two lines. 
-     *      • each row precedes a tab of space
-     *      • each character in the grid has one space from the other character
-     */        
-
-   /** Task 4 - Write a function that lets the user choose a spot
-     * Function name – askUser
-     * @param board (char[][] board)
-     * @return spot (int[])
+     * if (X) turn {
+     * Task 4: call askUser().
+     * Task 5: populate the board using askUser's return value.
+     * } else {
+     * Task 4: call askUser().
+     * Task 5: populate the board using askUser's return value. Then, print it.
      * 
-     * Inside the function
-     *   1. Asks the user: - pick a row and column number: 
-     *   2. Check if the spot is taken. If so, let the user choose again.
-     *   3. Return the row and column in an int[] array.
+     * }
      * 
+     * Task 6 - Call the function.
+     * if return value == 3 {
+     * print: X wins and break the loop
+     * } else if return value == -3 {
+     * print: O wins and break the loop
+     * }
+     * 
+     * }
      */
 
-    /** Task 6 - Write a function that determines the winner
-     * Function name - checkWin 
-     * @param board (char[][])
-     * @return count (int)
-     * 
-     * Inside the function:
-     *   1. Make a count variable that starts at 0.
-     *   2. Check every row for a straight X or straight O (Task 7).
-     *   3. Check every column for a straight X or straight O (Task 8).
-     *   4. Check the left diagonal for a straight X or straight O (Task 9).
-     *   5. Check the right diagonal for a straight X or straight O (Task 10).
-     */
+    scan.close();
+  }
 
+  public static void printBoard(char[][] board) {
+    System.out.print("\n");
+    for (int i = 0; i < board.length; i++) {
+      for (int j = 0; j < board[i].length; j++) {
+        System.out.print(board[i][j] + " ");
+      }
+      System.out.print("\n");
+    }
+  }
+
+  public static int[] askUser(char[][] board) {
+    System.out.print("Pick a row and column number:");
+
+    int row = scan.nextInt();
+    int column = scan.nextInt();
+
+    while (board[row][column] != '_') {
+      System.out.print("\nspot taken, try again:");
+      row = scan.nextInt();
+      column = scan.nextInt();
+    }
+
+    int[] spot = { row, column };
+    return spot;
+  }
+
+  /**
+   * Task 6 - Write a function that determines the winner
+   * Function name - checkWin
+   * 
+   * @param board (char[][])
+   * @return count (int)
+   *         int spot = new int[row, col];
+   *         Inside the function:
+   *         1. Make a count variable that starts at 0.
+   *         2. Check every row for a straight X or straight O (Task 7).
+   *         3. Check every column for a straight X or straight O (Task 8).
+   *         4. Check the left diagonal for a straight X or straight O (Task 9).
+   *         5. Check the right diagonal for a straight X or straight O (Task 10).
+   */
+
+  public static int checkWin(char[][] board) {
+    int count = 0;
+
+    // rows
+    if (board[0][0] == 'X' && board[0][1] == 'X' && board[0][2] == 'X') {
+      count = 3;
+    }
+    if (board[1][0] == 'X' && board[1][1] == 'X' && board[1][2] == 'X') {
+      count = 3;
+
+    }
+    if (board[2][0] == 'X' && board[2][1] == 'X' && board[2][2] == 'X') {
+      count = 3;
+
+    }
+    if (board[0][0] == 'O' && board[0][1] == 'O' && board[0][2] == 'O') {
+      count = -3;
+
+    }
+    if (board[1][0] == 'O' && board[1][1] == 'O' && board[1][2] == 'O') {
+      count = -3;
+
+    }
+    if (board[2][0] == 'O' && board[2][1] == 'O' && board[2][2] == 'O') {
+      count = -3;
+
+    }
+
+    // Diagonals
+
+    if (board[0][0] == 'X' && board[1][1] == 'X' && board[2][2] == 'X') {
+      count = 3;
+
+    }
+    if (board[0][2] == 'X' && board[1][1] == 'X' && board[2][0] == 'X') {
+      count = 3;
+
+    }
+    if (board[0][0] == 'O' && board[1][1] == 'O' && board[2][2] == 'O') {
+      count = -3;
+
+    }
+    if (board[0][2] == 'O' && board[1][1] == 'O' && board[2][0] == 'O') {
+      count = -3;
+
+    }
+
+    // Columns
+
+    if (board[0][0] == 'X' && board[1][0] == 'X' && board[2][0] == 'X') {
+      count = 3;
+
+    }
+    if (board[0][1] == 'X' && board[1][1] == 'X' && board[2][1] == 'X') {
+      count = 3;
+
+    }
+    if (board[0][2] == 'X' && board[1][2] == 'X' && board[2][2] == 'X') {
+      count = 3;
+
+    }
+    if (board[0][0] == 'O' && board[1][0] == 'O' && board[2][0] == 'O') {
+      count = -3;
+
+    }
+    if (board[0][1] == 'O' && board[1][1] == 'O' && board[2][1] == 'O') {
+      count = -3;
+
+    }
+    if (board[0][2] == 'O' && board[1][2] == 'O' && board[2][2] == 'O') {
+      count = -3;
+
+    }
+
+    if (count == 3 || count == -3) {
+      return count;
+    } else {
+      return 0;
+    }
+
+    // return count;
+  }
 
 }
