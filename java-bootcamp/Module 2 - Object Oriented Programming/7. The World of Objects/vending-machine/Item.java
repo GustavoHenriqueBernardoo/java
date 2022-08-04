e6e391a94fa725f7252f5d531609c7773fe35014
Gustavo Henrique Bernardo <gustavobernardo094@gmail.com>
2022-08-04 10:46:21 -0300

    vending challenge

--- java-bootcamp/Module 2 - Object Oriented Programming/7. The World of Objects/vending-machine/Item.java
+++ java-bootcamp/Module 2 - Object Oriented Programming/7. The World of Objects/vending-machine/Item.java
@@ -1,3 +1,52 @@
 public class Item {
-    
+
+  // fields
+  private String name;
+  private double price;
+  private int quantity;
+
+  // Constructor
+  public Item(String name, double price, int quantity) {
+    this.name = name;
+    this.price = price;
+    this.quantity = quantity;
+  }
+
+  // Copy Constructor
+  public Item(Item source) {
+    this.name = source.name;
+    this.price = source.price;
+    this.quantity = source.quantity;
+  }
+
+  // setters
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public void setPrice(double price) {
+    this.price = price;
+  }
+
+  public void setQuantity(int quantity) {
+    this.quantity = quantity;
+  }
+
+  // getters
+  public String getName() {
+    return this.name;
+  }
+
+  public double getPrice() {
+    return this.price;
+  }
+
+  public int getQuantity() {
+    return this.quantity;
+  }
+
+  public String toString() {
+
+    return this.getName() + ": " + this.getPrice() + " (" + this.quantity + ")";
+  }
 }
--- java-bootcamp/Module 2 - Object Oriented Programming/7. The World of Objects/vending-machine/Main.java
+++ java-bootcamp/Module 2 - Object Oriented Programming/7. The World of Objects/vending-machine/Main.java
@@ -1,15 +1,37 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
 
         System.out.println("\t************************************************");
         System.out.println("\t             WELCOME TO JAVA DRINKS!            ");
         System.out.println("\t************************************************");
-        
-        /* Item[][] items = new Item[][] {
-        /     { new Item("Pepsi", 1.99, 3) , new Item("Fresca", 1.49, 3), new Item("Brisk", 2.49, 2) },
-        /     { new Item("Fanta", 1.99, 2) , new Item("Barq's", 1.49, 2), new Item("A & W", 2.49, 3) },
-        /     { new Item("Crush", 1.99, 2) , new Item("C-Cola", 1.49, 2), new Item("Berry", 2.49, 1) }
-        / };
-        */
+
+        // Item item = new Item("Coca", 7.00, 100);
+
+        Item[][] items = new Item[][] {
+                { new Item("Pepsi", 1.99, 3), new Item("Fresca", 1.49, 3), new Item("Brisk", 2.49, 2) },
+                { new Item("Fanta", 1.99, 2), new Item("Barq's", 1.49, 2), new Item("A & W", 2.49, 3) },
+                { new Item("Crush", 1.99, 2), new Item("C-Cola", 2.99, 2), new Item("Berry", 2.49, 1) }
+        };
+
+        Machine machine = new Machine(items);
+
+        Item item = machine.getItems(0, 0);
+
+        item.setPrice(9.99);
+        // item.setName("Dolly Guarana");
+        item.setQuantity(10);
+
+        machine.setItems(item, 0, 0);
+
+        machine.dispense(0, 0);
+        machine.dispense(0, 0);
+        // System.out.println(machine.getItems(0, 0) + "\n");
+
+        // Item item = new Item(source)
+
+        System.out.println(machine);
+
     }
 }
