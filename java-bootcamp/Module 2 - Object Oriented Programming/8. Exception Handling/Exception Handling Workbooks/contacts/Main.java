6a324c0864094a1bb3f10f9404d36bf86bda8434
Gustavo Henrique Bernardo <gustavobernardo094@gmail.com>
2022-08-11 10:30:09 -0300

    bulletprof

--- java-bootcamp/Module 2 - Object Oriented Programming/8. Exception Handling/Exception Handling Workbooks/contacts/Main.java
+++ java-bootcamp/Module 2 - Object Oriented Programming/8. Exception Handling/Exception Handling Workbooks/contacts/Main.java
@@ -16,7 +16,9 @@ public class Main {
         try {
             loadContacts("contacts.txt");
 
-            System.out.println(contactManager);
+            System.out.println("Contact Loaded");
+
+            manageContacts();
 
         } catch (FileNotFoundException e) {
             System.out.println(e.getMessage());
@@ -26,11 +28,11 @@ public class Main {
 
     public static void loadContacts(String fileName) throws FileNotFoundException {
         FileInputStream fis = new FileInputStream(fileName);
-        Scanner scan = new Scanner(fis);
+        Scanner scanDocument = new Scanner(fis);
 
-        while (scan.hasNextLine()) {
+        while (scanDocument.hasNextLine()) {
             try {
-                Contact contact = new Contact(scan.next(), scan.next(), scan.next());
+                Contact contact = new Contact(scanDocument.next(), scanDocument.next(), scanDocument.next());
                 contactManager.addContact(contact);
 
             } catch (ParseException e) { // Parse Exception
@@ -39,7 +41,69 @@ public class Main {
             }
         }
 
+        scanDocument.close();
+    }
+
+    public static void manageContacts() {
+        Scanner scan = new Scanner(System.in);
+
+        try {
+            while (true) {
+                System.out.println("Would you like to \n\ta) add another contact\n\tb) remove a contact \n\tc) exit");
+                char option = scan.next().charAt(0);
+                // throw an next line because of the scan.next
+                scan.nextLine();
+                if (option == 'a') {
+                    // pick up name.
+                    System.out.println("\tName: ");
+                    String name = scan.nextLine();
+
+                    // pick up number.
+                    System.out.println("\tPhone Number: ");
+                    String phoneNumber = scan.nextLine();
+
+                    // pick up birth date.
+                    System.out.println("\tBirth Date: ");
+                    String dateOfBirth = scan.nextLine();
+
+                    if (name.isBlank() || name == null || phoneNumber.isEmpty() || phoneNumber == null
+                            || phoneNumber.length() < 5 || dateOfBirth.isEmpty() || dateOfBirth == null) {
+                        System.out.println("\nThe input you provided is not valid. Registration failed.\n");
+                        continue;
+                    }
+
+                    Contact newContact = new Contact(name, phoneNumber, dateOfBirth);
+                    contactManager.addContact(newContact);
+
+                    System.out.println("\n\nUPDATED CONTACTS \n\n ");
+                    System.out.println(contactManager);
+
+                }
+                if (option == 'b') {
+                    System.out.println("\nWho would you like to remove?");
+                    String name = scan.nextLine();
+
+                    contactManager.removeContact(name);
+
+                    System.out.println("\n\nUPDATED CONTACTS \n\n ");
+                    System.out.println(contactManager);
+                }
+                if (option == 'c') {
+                    break;
+                }
+            }
+        } catch (ParseException e) {
+            // TODO: handle exception
+            System.out.println(e.getMessage());
+        } finally {
+            System.out.println("\n\nProcess Complete \n\n ");
+            System.out.println("\n\nHeres' how your contact are: ");
+
+            System.out.println(contactManager);
+        }
+
         scan.close();
+
     }
 
 }
