75a22039674d4a778fec69da340d48dee47307dc
Gustavo Henrique Bernardo <gustavobernardo094@gmail.com>
2022-08-13 11:29:12 -0300

    add: loadItem method to read a doc and set items into the store class

--- java-bootcamp/Module 2 - Object Oriented Programming/8. Exception Handling/Challenge/shopping-cart/Main.java
+++ java-bootcamp/Module 2 - Object Oriented Programming/8. Exception Handling/Challenge/shopping-cart/Main.java
@@ -1,29 +1,27 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Scanner;
+
 import models.*;
 
 public class Main {
+    static Store store = new Store();
+
+    static Cart cart = new Cart();
 
     public static void main(String[] args) {
-        Item[][] inventory = new Item[][] {
-                { new Item("Pepsi", 1.99), new Item("Crush", 1.99), new Item("Cola", 1.99) },
-                { new Item("Honey Oats", 3.99), new Item("Fruit Loops", 1.99), new Item("Cheerios", 2.99) },
-                { new Item("Milk", 4.99), new Item("Eggs", 0.99), new Item("Cheese", 1.89) },
-                { new Item("Pepperoni", 2.99), new Item("Salami", 4.49), new Item("Mortadella", 4.99) },
-                { new Item("Celery", 0.99), new Item("Spinach", 0.99), new Item("Coriander", 1.29) },
-                { new Item("Shirt", 12.99), new Item("Pants", 24.99), new Item("Sweater", 18.99) },
-                { new Item("Phone", 549.99), new Item("Printer", 349.99), new Item("Television", 1099) }
-        };
 
-        Store store = new Store();
+        try {
+            loadItem("products.txt");
 
-        for (int i = 0; i < inventory.length; i++) {
-            for (int j = 0; j < inventory[i].length; j++) {
-                store.setItem(i, j, inventory[i][j]);
-            }
+            System.out.println(store);
+        } catch (FileNotFoundException e) {
+            // TODO: handle exception
+            System.out.println(e.getMessage());
         }
-
-        System.out.println("\n\t******************************JAVA GROCERS******************************\n");
-
-        System.out.println(store + "\n");
 
     }
 
@@ -38,6 +36,31 @@ public class Main {
      * • case c: prints the receipt and closes Scanner.
      * • Prints the updated shopping cart.
      */
+
+    public static void loadItem(String fileName) throws FileNotFoundException {
+        FileInputStream fis = new FileInputStream(fileName);
+        Scanner scanTxt = new Scanner(fis);
+
+        int count = 0;
+        while (scanTxt.hasNextLine()) {
+
+            String line = scanTxt.nextLine();
+            String[] lineItem = line.split(";");
+
+            for (int i = 0; i < lineItem.length; i++) {
+
+                String[] item = lineItem[i].split("=");
+
+                String name = item[0];
+                double price = Double.parseDouble(item[1]);
+
+                store.setItem(count, i, new Item(name, price));
+            }
+
+            count++;
+        }
+
+    }
 
     /**
      * Name: loadItems
