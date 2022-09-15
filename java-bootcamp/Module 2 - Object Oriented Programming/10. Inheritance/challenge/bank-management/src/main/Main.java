9b606d8614d6a3f634f48dfbc351f9c3175ff6b7
Gustavo Henrique Bernardo <gustavobernardo094@gmail.com>
2022-09-15 16:55:15 -0300

    set up main

--- java-bootcamp/Module 2 - Object Oriented Programming/10. Inheritance/challenge/bank-management/src/main/Main.java
+++ java-bootcamp/Module 2 - Object Oriented Programming/10. Inheritance/challenge/bank-management/src/main/Main.java
@@ -1,8 +1,14 @@
 package src.main;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
+
+import javax.print.event.PrintEvent;
 
 import src.main.model.Bank;
 import src.main.model.Transaction;
@@ -19,7 +25,61 @@ public class Main {
 
     public static void main(String[] args) {
 
+        try {
+            Account account = createObject(
+                    new String[] { "Chequing", "f84c43f4-a634-4c57-a644-7602f8840870", "Michael Scott", "1524.51" });
+
+            System.out.println();
+
+            returnAccounts();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
     }
+
+    public static Account createObject(String[] values) throws Exception {
+
+        return (Account) Class.forName("src.main.model.account." + values[0])
+                .getConstructor(String.class, String.class, double.class)
+                .newInstance(values[1], values[2], Double.parseDouble(values[3]));
+
+    }
+
+    public static ArrayList<Account> returnAccounts() throws FileNotFoundException {
+        ArrayList<Account> accounts = new ArrayList<>();
+
+        FileInputStream fis = new FileInputStream(ACCOUNTS_FILE);
+        Scanner scan = new Scanner(fis);
+        for (int i = 0; scan.hasNextLine(); i++) {
+            String line = scan.nextLine();
+            String[] account = line.split(" ");
+            try {
+
+                Account newAccount = createObject(account);
+                accounts.add(newAccount);
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+
+        }
+
+        return accounts;
+    }
+
+    /**
+     * Name: returnAccounts()
+     * 
+     * @return ArrayList<Account>
+     * @throws FileNotFoundException
+     * 
+     *                               Inside the function:
+     *                               1. Creates a Scanner object and reads the data
+     *                               from accounts.txt.
+     *                               2. Creates an Account object for every line in
+     *                               accounts.txt.
+     *                               3. Returns an ArrayList of Account objects.
+     */
 
     /**
      * Function name: wait
