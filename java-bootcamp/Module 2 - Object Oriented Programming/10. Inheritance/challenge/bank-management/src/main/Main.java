00537a4b78fef08df76facaf3f8ebae66330931a
Gustavo Henrique Bernardo <gustavobernardo094@gmail.com>
2022-09-16 14:37:51 -0300

    finish application

--- java-bootcamp/Module 2 - Object Oriented Programming/10. Inheritance/challenge/bank-management/src/main/Main.java
+++ java-bootcamp/Module 2 - Object Oriented Programming/10. Inheritance/challenge/bank-management/src/main/Main.java
@@ -26,23 +26,32 @@ public class Main {
     public static void main(String[] args) {
 
         try {
-            Account account = createObject(
-                    new String[] { "Chequing", "f84c43f4-a634-4c57-a644-7602f8840870", "Michael Scott", "1524.51" });
+            ArrayList<Account> accounts = returnAccounts();
+            loadAccounts(accounts);
 
-            System.out.println();
+            ArrayList<Transaction> transactions = returnTransactions();
+            runTransactions(transactions);
+            bank.deductTaxes();
+            for (Account account : accounts) {
+                System.out.println("\n\t\t\t\t\t ACCOUNT\n\n\t" + account + "\n\n");
+                transactionHistory(account.getId());
+            }
 
-            returnAccounts();
-        } catch (Exception e) {
+        } catch (FileNotFoundException e) {
             System.out.println(e.getMessage());
         }
-
     }
 
-    public static Account createObject(String[] values) throws Exception {
+    public static Account createObject(String[] values) {
 
-        return (Account) Class.forName("src.main.model.account." + values[0])
-                .getConstructor(String.class, String.class, double.class)
-                .newInstance(values[1], values[2], Double.parseDouble(values[3]));
+        try {
+            return (Account) Class.forName("src.main.model.account." + values[0])
+                    .getConstructor(String.class, String.class, double.class)
+                    .newInstance(values[1], values[2], Double.parseDouble(values[3]));
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return null;
+        }
 
     }
 
@@ -51,35 +60,59 @@ public class Main {
 
         FileInputStream fis = new FileInputStream(ACCOUNTS_FILE);
         Scanner scan = new Scanner(fis);
-        for (int i = 0; scan.hasNextLine(); i++) {
+
+        while (scan.hasNextLine()) {
             String line = scan.nextLine();
-            String[] account = line.split(" ");
-            try {
+            String[] account = line.split(",");
 
-                Account newAccount = createObject(account);
-                accounts.add(newAccount);
-            } catch (Exception e) {
-                System.out.println(e.getMessage());
-            }
-
+            Account newAccount = createObject(account);
+            accounts.add(newAccount);
         }
 
         return accounts;
     }
 
-    /**
-     * Name: returnAccounts()
-     * 
-     * @return ArrayList<Account>
-     * @throws FileNotFoundException
-     * 
-     *                               Inside the function:
-     *                               1. Creates a Scanner object and reads the data
-     *                               from accounts.txt.
-     *                               2. Creates an Account object for every line in
-     *                               accounts.txt.
-     *                               3. Returns an ArrayList of Account objects.
-     */
+    public static void loadAccounts(ArrayList<Account> accounts) {
+        for (Account account : accounts) {
+            bank.addAccount(account);
+        }
+    }
+
+    public static ArrayList<Transaction> returnTransactions() throws FileNotFoundException {
+        FileInputStream fis = new FileInputStream(TRANSACTIONS_FILE);
+        Scanner scan = new Scanner(fis);
+
+        ArrayList<Transaction> transactions = new ArrayList<>();
+
+        while (scan.hasNextLine()) {
+            String line = scan.nextLine();
+            String[] lineArr = line.split(",");
+            Transaction transaction = new Transaction(Transaction.Type.valueOf(lineArr[1]), Long.parseLong(lineArr[0]),
+                    lineArr[2],
+                    Double.parseDouble(lineArr[3]));
+            transactions.add(transaction);
+        }
+        return transactions;
+    }
+
+    public static void runTransactions(ArrayList<Transaction> transactions) {
+        for (Transaction transaction : transactions) {
+            bank.executeTransaction(transaction);
+        }
+    }
+
+    public static void transactionHistory(String id) {
+        System.out.println("\t\t\t\t TRANSACTION HISTORY\n\t");
+        Transaction[] transactions = bank.getTransactions(id);
+        for (int i = 0; i < transactions.length; i++) {
+            System.out.println("\t" + transactions[i] + "\n");
+            wait(300);
+        }
+        System.out.println("\n\t\t\t\t\tAFTER TAX\n");
+
+        System.out.println(bank.getAccount(id));
+
+    }
 
     /**
      * Function name: wait
